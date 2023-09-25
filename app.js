e517f0fb39c75d9d31bcbafd12a10756e9fe2903
YaroslavHolovatyi <yaroslavgolovatyy@gmail.com>
2023-09-25 19:42:30 +0300

    change of the app.js for cors policy

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(corsOptions))
 app.use("/", router);
-app.get('/', (req, res) => res.json('Equivalence API working'))
+app.get('/', (req, res) => res.json('Equivalence API working ', corsOptions.origin))
 
 
 mongoose
