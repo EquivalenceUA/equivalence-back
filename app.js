32f12e9b6a11555a5373e524525bf7066cb3fc0d
YaroslavHolovatyi <yaroslavgolovatyy@gmail.com>
2023-09-25 17:04:17 +0300

    changed app.js

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 
+const corsOptions = {
+  origin: 'http://localhost:4200'
+  };
+
 const app = express();
 
 
 app.use(express.json());
 app.use(cors(corsOptions))
-app.use("/api/users", router);
+app.use("/", router);
 app.get('/', (req, res) => res.json('Equivalence API working'))
 
 
 mongoose
     .connect(
-      MONGO_URL
+        "mongodb+srv://admin:Barabolya987^@cluster0.i2iiqvu.mongodb.net/?retryWrites=true&w=majority"
     )
     .then(()=>app.listen(port))
     .then(()=>
